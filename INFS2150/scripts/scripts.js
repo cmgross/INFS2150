8e46b3b112957bd095912b11d24fe0f7cdbcd768
Uziel <chuckgross@gmail.com>
2013-11-11 17:32:06 -0500

    Moved links into script

---
+++ INFS2150/scripts/scripts.js
@@ -0,0 +1,10 @@
+﻿function generateMenu() {
+    var links = "<li><a href='index.html'>Home</a></li>" +
+        "<li><a href='throws.html'>Throws</a></li>" +
+        "<li><a href='contact.html'>Classes</a></li>" +
+        "<li><a href='contact.html'>Kilts</a></li>" +
+        "<li><a href='contact.html'>Organizations</a></li>" +
+        "<li><a href='contact.html'>Festivals</a></li>" +
+        "<li><a href='contact.html'>Sign Up</a></li>";
+    document.getElementById("mainnav").innerHTML = links;
+}
